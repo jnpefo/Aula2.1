cf9d284e9fa1a466d1635a2b52022da3f8978db5
jnpefo <jnpefo@gmail.com>
2020-10-26 18:26:18 -0300

    Fiz até o 5, vou terminar depois

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Exercicio2.js
@@ -0,0 +1,10 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+
+let a = 7, b = 6;
+
+if (a<b){
+    console.log("O maior dois dois números é: ", b);
+} else {
+    console.log("O maior dois dois números é: ", a);
+}
