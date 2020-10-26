6ac493665fa2be84a5b88e8a82a5965ffef5ca1a
jnpefo <jnpefo@gmail.com>
2020-10-26 18:21:47 -0300

    Fiz até o 5, vou terminar depois

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Exercicio1.js
@@ -0,0 +1,24 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+let a = 7, b = 6, result;
+
+// Adição
+result = a + b;
+console.log(result);
+
+// Subtração
+result = a - b;
+console.log(result);
+
+// Multiplicação
+result = a * b;
+console.log(result);
+
+// Divisão
+result = a / b;
+console.log(result);
+
+// Módulo
+result = a % b;
+console.log(result);
+
