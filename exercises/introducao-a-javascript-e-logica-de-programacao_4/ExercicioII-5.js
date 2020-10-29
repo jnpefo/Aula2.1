e0d75f021e7f1943fb234b6648046c0d6381ceca
jnpefo <jnpefo@gmail.com>
2020-10-29 14:13:37 -0300

    feito parte do exercicio 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/ExercicioII-5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/ExercicioII-5.js
@@ -3,13 +3,24 @@
 // Valor esperado no retorno da função: 2
 
 
-let teste = [2, 3, 2, 5, 8, 2, 3], comp = 0, maior = 0;
+let teste = [2, 3, 2, 3, 3, 2, 3], comp = 0;
+let obj = {
 
-for (let index = 0; index < teste.length; index += 1){
-    comp= teste[index];
-    if(){
-        
+};
+
+for (let index = 0; index < teste.length; index += 1) {
+    if (obj[teste[index]] == undefined) {
+        obj[teste[index]] = 1;
+    } else {
+        obj[teste[index]] += 1;
     }
 }
+for (let cont = 0; cont < obj.length; cont += 1){
+    if(obj[cont){
+        
+    }
+    
+}
 
-console.log(comp);
+
+console.log(obj);
