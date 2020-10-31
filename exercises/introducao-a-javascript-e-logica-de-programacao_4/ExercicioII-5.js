ea69132a67a3b7d24625f99c737aeac7a7889905
jnpefo <jnpefo@gmail.com>
2020-10-31 19:02:55 -0300

    Salvando documentos

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/ExercicioII-5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/ExercicioII-5.js
@@ -3,7 +3,8 @@
 // Valor esperado no retorno da função: 2
 
 
-let teste = [2, 3, 2, 3, 3, 2, 3], comp = 0;
+let teste = [9, 1, 2, 3, 9, 5, 7], maior = 0;
+let key;
 let obj = {
 
 };
@@ -15,12 +16,15 @@ for (let index = 0; index < teste.length; index += 1) {
         obj[teste[index]] += 1;
     }
 }
-for (let cont = 0; cont < obj.length; cont += 1){
-    if(obj[cont){
-        
-    }
+
+for (let chave in obj) {
     
+    if (obj[chave] > maior) {
+        key = chave;
+        maior = obj[chave];
+    }
 }
 
 
+console.log(key)
 console.log(obj);
