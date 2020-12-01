d6c904165dddfe92e1180b37e206c2acc4691adb
jnpefo <jnpefo@gmail.com>
2020-11-30 21:46:23 -0300

    tentando o 7

--- bloco_8/exercises/higher-order-functions-do-javascript-es6_1/exercicio7.js
+++ bloco_8/exercises/higher-order-functions-do-javascript-es6_1/exercicio7.js
@@ -68,10 +68,11 @@ const expectedResult = false;
 // Faça uma função que retorne true, caso nenhum author tenha nascido no mesmo ano, e false, caso contrário.
 
 function authorUnique(books) {
-  return books.every((book) =>
-    !books.some((bookSome) =>
-      (bookSome.author.birthYear === book.author.birthYear) && (bookSome.id !== book.id)
-    ));
+  // const result = books.every(book => (
+  //   book.author.birthYear > 1901 && book.author.birthYear <= 2000
+  // ));
+  // console.log(result);
+  // return result;
 }
 
 assert.equal(authorUnique(books), expectedResult);
