aac96c47133e32d8e9fa33c94859ab3ece624640
jnpefo <jnpefo@gmail.com>
2020-12-08 23:27:15 -0300

    terminando por hoje o bloco 9 dia 1

---
+++ bloco_9/exercises/javascript-assincrono-e-promises_1/4.js
@@ -0,0 +1,24 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9 / 5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const sendMarsTemperature = (onSuccess) => {
+  const currentTemperature = getMarsTemperature();
+  setTimeout(() => onSuccess(currentTemperature), messageDelay());
+};
+
+sendMarsTemperature(temperatureInFahrenheit);
+sendMarsTemperature(greet);; // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+
+
+// Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio. Crie a função sendMarsTemperature , que imprime a temperatura em Marte.
