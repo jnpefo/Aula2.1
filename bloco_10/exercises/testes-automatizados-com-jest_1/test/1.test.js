f431f286d258138982473931419d42c66084f309
jnpefo <jnpefo@gmail.com>
2021-01-05 00:03:27 -0300

    parando por hoje

--- bloco_10/exercises/testes-automatizados-com-jest_1/test/1.test.js
+++ bloco_10/exercises/testes-automatizados-com-jest_1/test/1.test.js
@@ -18,12 +18,15 @@ describe('Parte 1', () => {
     expect(sum(0, 0)).toBe(0);
   });
 
-  // test('Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)', () => {
-  //   expect(sum(4, '5')).toThrow('parameters must be numbers');
-  // });
+  test('Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)', () => {
+    expect(() => {
+      (sum(4, '5')).toThrow('parameters must be numbers');
+    })
+  });
 
-  // test('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
-  //   expect(sum(4, '5')).toThrow();
-  // });
-
+  test('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => {
+      sum(4, '5').toThrow();
+    })
+  });
 });
