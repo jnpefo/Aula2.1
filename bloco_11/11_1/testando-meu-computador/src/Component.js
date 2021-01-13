40e5da3bea75e925fcd8ae9a4291cc100b247747
jnpefo <jnpefo@gmail.com>
2021-01-13 17:10:39 -0300

    Req 1, teste1

---
+++ bloco_11/11_1/testando-meu-computador/src/Component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Jean Paternostre Franco</h1>
+        <p>Aprendendo a programar</p>
+      </div>
+    );
+  };
+};
+
+export default Component
