af10480ef7364ac945fbf84c2aa18566ee57d1f2
jnpefo <jnpefo@gmail.com>
2021-01-25 10:05:28 -0300

    limpando

--- bloco_11/exercises/introducao-a-react_1/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  const tarefas = ['acordar', 'trabalhar', 'comer', 'voltarCasa', 'dormir'];
-  const task = value => {
-    return <li>{value}</li>;
-  };
-  return tarefas.map(task);
-}
-
-export default App;
