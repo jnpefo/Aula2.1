0a1ba059c10c200b410e01c41bbe10c4506207ea
jnpefo <jnpefo@gmail.com>
2021-02-04 18:51:21 -0300

    Indo para o exercicio

---
+++ bloco_15/15_1/testes-react/src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './App.css';
+import ValidEmail from './ValidEmail';
+import Mensagem from './Mensagem';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      saveEmail: '',
+    };
+  }
+
+  changeEmail(value) {
+    this.setState({ email: value });
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ saveEmail: value, email: '' });
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    if(!email) {
+      return (
+        <div className="App">
+          <label htmlFor="id-email">
+            Email
+          </label>
+          <input
+            id="id-email"
+            value={email}
+            type="email"
+            onChange={(e) => this.changeEmail(e.target.value)}
+          />
+          <input
+            id="btn-enviar"
+            type="button"
+            data-testid="id-send"
+            value="Enviar"
+            onClick={() => this.changeSaveEmail(email)}
+          />
+          <input id="btn-id" type="button" value="Voltar" />
+          <Mensagem />
+          <ValidEmail email={saveEmail}/>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <label htmlFor="id-email">
+            Email
+          </label>
+          <input
+            id="id-email"
+            value={email}
+            type="email"
+            onChange={(e) => this.changeEmail(e.target.value)}
+          />
+          <input
+            id="btn-enviar"
+            type="button"
+            data-testid="id-send"
+            value="Enviar"
+            onClick={() => this.changeSaveEmail(email)}
+          />
+          <input id="btn-id" type="button" value="Voltar" />
+          <ValidEmail email={saveEmail}/>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
