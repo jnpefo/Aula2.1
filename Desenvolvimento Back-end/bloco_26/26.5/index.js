117a90df68e5e829496632f3e81d1b6d80fe6e14
jnpefo <jnpefo@gmail.com>
2021-05-20 20:38:05 -0300

    fix

--- Desenvolvimento Back-end/bloco_26/26.5/index.js
+++ Desenvolvimento Back-end/bloco_26/26.5/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Deve receber no campo status um boleano e alterar o status do usuário correspôndente. Se o campo status não for um boleano, deve retornar o error "status isn't a boolean". Caso nao exista usuário correspondente, deve rotornar o error "user isn't found".
 const users = [
