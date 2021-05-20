05b90b3a9b29ab0304dee226b437f4f0ca9ce208
jnpefo <jnpefo@gmail.com>
2021-05-20 18:52:54 -0300

    Req 9

---
+++ Desenvolvimento Back-end/bloco_26/26.5/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+
+// Deve receber no campo status um boleano e alterar o status do usuário correspôndente. Se o campo status não for um boleano, deve retornar o error "status isn't a boolean". Caso nao exista usuário correspondente, deve rotornar o error "user isn't found".
+const users = [
+  {
+    id: 1,
+    user: 'marcos',
+    isActive:true
+  },
+  {
+    id: 2,
+    user: 'paulo',
+    isActive:true
+
+  },
+  {
+    id: 3,
+    user: 'roger',
+    isActive:false
+  }
+]
+
+app.put('/user/:id', (req, res, _next) => {
+  const result = users.find((user) => user.id == req.params.id )
+  const { status } = req.body
+  console.log(status)
+  if(result === undefined) {
+    return res.status(400).json({ error: "user isn't found" })
+  } 
+  if(typeof result.isActive !== "boolean") {
+    return res.status(400).json({ error: "status isn't a boolean" })
+  } 
+  result.isActive = status
+  res.status(200).json({ result })
+})
+
+
+app.listen(3000, () => {
+  console.log('listen on 3000');
+})
