9cc8673b0e4fbd818fd46df75ae53229c72b2fc7
jnpefo <jnpefo@gmail.com>
2021-07-06 19:40:51 -0300

    Dia 31.2

---
+++ Desenvolvimento Back-end/bloco_31/31.2/fixacao e exercicio/projeto/client.js
@@ -0,0 +1,15 @@
+const net = require('net');
+
+const client = net.connect({ port: 8080 }, () => {
+  console.log('Cliente conectado ao servidor!');
+  client.write('Cliente número 1');
+});
+
+client.on('data', (data) => {
+  console.log(data.toString());
+  // client.end();
+});
+
+// client.on('end', () => {
+//   console.log('Desconectado do servidor');
+// });
