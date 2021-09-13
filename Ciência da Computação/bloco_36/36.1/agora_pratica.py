5343d8c3cb21bc59d7ca01b97fd58302b536fd61
jnpefo <jnpefo@gmail.com>
2021-09-13 16:27:20 -0300

    parando o dia 36.1

---
+++ Ciência da Computação/bloco_36/36.1/agora_pratica.py
@@ -0,0 +1,16 @@
+# Vamos colocar tudo o que vimos até agora em prática.
+# Exercício 1 Dado um array de números de tamanho n , escreva um algoritmo que retorna true se há no array um número duplicado e false caso contrário. Analise a solução abaixo e diga qual é a ordem de complexidade desse algoritmo para o melhor caso, pior caso e caso médio.
+
+def contains_duplicate(numbers):
+    numbers.sort()
+    previous_number = 'not a number';
+    for number in numbers:
+        if(previous_number == number): return True
+        previous_number = number
+
+    return False
+
+# meu_array = list(range(1, 200))
+meu_array = [1, 1, 3, 5, 6, 7]
+
+print(contains_duplicate(meu_array))
