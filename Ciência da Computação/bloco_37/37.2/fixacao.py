b860916a1edbf5072967d82fc8e510ebbb0e8729
jnpefo <jnpefo@gmail.com>
2021-09-27 16:22:47 -0300

    Parando dia 37.2

---
+++ Ciência da Computação/bloco_37/37.2/fixacao.py
@@ -0,0 +1,6 @@
+# Exercício 1 Tipos abstratos de dados possuem uma única implementação bem documentada?
+
+# R: Não
+
+# Exercício 2 Listas( list ), dicionários( dict ) e conjuntos( set ), que já vêm no Python, são considerados tipos de dados? Caso negativo, justifique sua resposta.
+# R: Sim
